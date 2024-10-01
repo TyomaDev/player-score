e23c6124b26245bc2df474e40e4379846fa4b266
Программист Ортем <88384382+TyomaDev@users.noreply.github.com>
2024-10-01 17:51:52 +0300

    fix spelling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import GameDataHandler
 
 if __name__ == '__main__':
-    print(f"Средний бал участников- {GameDataHandler.get_average_player_score('DATA.csv')}")
-    print(f"Средний бал в раундах- {GameDataHandler.get_average_round_score('DATA.csv')}")
+    print(f"Средний балл участников- {GameDataHandler.get_average_player_score('DATA.csv')}")
+    print(f"Средний балл в раундах- {GameDataHandler.get_average_round_score('DATA.csv')}")
